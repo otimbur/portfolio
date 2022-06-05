1fbd3aab8ebc2293c7d53ef3d5f7ec950256d4aa
otimbur <otimbur@sea.yearup.org>
2022-06-04 21:33:22 -0700

    change content

--- app.js
+++ app.js
@@ -1,33 +1,10 @@
+const toggleButton = document.querySelector('#toggle');
+const navbar = document.querySelector('.navbar');
 
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+toggleButton.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
 
-    burger.addEventListener('click', () => {
-        //toggle nav
-        nav.classList.toggle('nav-active');
-
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
-            }
-
-        });
-
-        //burger animation
-        burger.classList.toggle('toggle');
-
-
-
-    });
-
-}
-
-navSlide();
 
 
 // References to DOM Elements
@@ -113,3 +90,4 @@ function goPrevPage() {
         currentLocation--;
     }
 }
+
